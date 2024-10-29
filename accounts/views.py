16e8dffde9eb218bbc46a8af3927e0458e91e648
Adnan Moghal <theadnanmoghal@gmail.com>
2024-10-29 17:50:38 +0500

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    # Implement your login logic here
+    pass
